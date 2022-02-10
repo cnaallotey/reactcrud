791b3a34339c94a0e5a75d995d04d851b7cd2273
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-10 19:42:18 +0000

    make github repo button work

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,8 +1,18 @@
+import githubImage from './github (1).svg'
+
+
+
 const navbar = () => {
+    const github = () => {
+        window.open("https://github.com/cnaallotey/reactcrud" , "_blank")
+    }
+
+
     return (
         <div className="Navbar w-full py-5 bg-slate-900"><div className=" max-w-screen-xl mx-auto px-5 xl:px-0"> <div className="w-full flex items-center justify-between">
             <h2 className="text-amber-600 font-bold text-3xl">CRUD react</h2>
-            <button className="py-3 px-5 text-white"><span>Visit repo</span></button>
+            <button className="py-3 px-5 text-white flex items-center rounded-md bg-black bg-opacity-0 hover:bg-opacity-80 transition ease-out select-none delay-100 " onClick={github}>
+                <img src={githubImage} className="w-10 h-10 mr-2" alt="github" /><span className="hidden md:contents">Visit repo</span></button>
         </div></div></div>
     )
 }
